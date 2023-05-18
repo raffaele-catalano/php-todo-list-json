3582ad025ea0ba1452dc9c5b2165ed07ee9244d8
raffaele-catalano <raf.catalano@gmail.com>
2023-05-18 16:22:40 +0200

    json decoded and encoded

--- server.php
+++ server.php
@@ -5,5 +5,9 @@ $json_object = file_get_contents('db.json');
 
 $task_list = json_decode($json_object);
 
-var_dump($task_list);
+// var_dump($task_list);
+
+header('Content-Type: application/json');
+
+echo json_encode($task_list);
 ?>
